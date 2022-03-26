bedae7782969cd0f42cab0a0966df8156967ded9
Avantika150711 <91789969+Avantika150711@users.noreply.github.com>
2022-03-26 16:55:42 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,54 @@
+img="";
+objects=[];
+status="";
+objectDetector=""; 
+
+function preload() {
+    img=loadImage('box img.jpg');
+}
+
+function setup() {
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status : detecting objects";
+}
+function modelLoaded() {
+    console.log("modelLoaded");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
+
+
+
+function draw() {
+    
+
+    if (status !="") {
+        image(img,0,0,640,420);
+        for (var i=0; i< objects.length; i++) {
+            document.getElementById("status").innerHTML="status : object detected.";
+
+            fill(255,0,0);
+            percent=floor(objects[i].confidence*100);
+    text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+    noFill();
+    stroke(255,0,0);
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    
+    }
+
+    
+}
+function box() {
+    window.location="box.html";
+}
